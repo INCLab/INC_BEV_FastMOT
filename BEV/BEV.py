c660098a8c7cd6adc626fe8866a67df1bbb8ecf2
Aftermoon-dev <ddol0225@gmail.com>
2021-09-21 23:04:25 +0900

    Applied BEV (Not Completed) in startall.py

--- BEV/BEV.py
+++ BEV/BEV.py
@@ -1,12 +1,12 @@
-import numpy as np
-from function import PixelMapper
-from function import save_lonlat_frame
-from function import save_dict
-from function import getcolor
 import cv2
 import os
 import sys
 import shutil
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
 ##############################################################################
 
 '''
@@ -15,130 +15,250 @@ lonloat : 도면 공간
 실제 mapping 되는 곳에 좌표를 입력 @@@.py 사용
 오른쪽 위, 왼쪽 위, 왼쪽 아래, 오른쪽 아래 순서
 '''
-input_path = sys.argv[1]
-output_path = sys.argv[2]
-heatmap_path = os.path.join(output_path, 'heatmap.png')
-output_path = os.path.join(output_path, 'map_frame')
-map_path = sys.argv[3]
-temp_path = "./temp"
 
-if not os.path.exists(output_path):
-    os.makedirs(output_path)
-else:
-    shutil.rmtree(output_path)
-    os.makedirs(output_path)
+def start(input_path, output_path, map_path):
+    heatmap_path = os.path.join(output_path, 'heatmap.png')
+    original_output_path = output_path
+    output_path = os.path.join(output_path, 'map_frame')
+    temp_path = "./temp"
 
-num = len(os.listdir(input_path))
-f = open(os.path.join(temp_path, 'points.txt'), 'r')
-data = f.read()
-data = data.split('\n')
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    else:
+        shutil.rmtree(output_path)
+        os.makedirs(output_path)
 
-point = []
-frame_point = []
-map_point = []
+    num = 0
+    for filename in os.listdir(input_path):
+        name, ext = os.path.splitext(filename)
+        if ext == '.mp4':
+            num += 1
 
-for i in data:
-    point.append(i.split(' '))
+    f = open(os.path.join(temp_path, 'points.txt'), 'r')
+    data = f.read()
+    data = data.split('\n')
 
-for i in point:
-    if i[0] == 'map':
-        map_point.append(i)
-    elif i[0] == 'frame':
-        frame_point.append(i)
+    point = []
+    frame_point = []
+    map_point = []
 
-if not(len(map_point) % 4 == 0 and len(frame_point) % 4 == 0):
-    print('Point Error')
-    sys.exit()
+    for i in data:
+        point.append(i.split(' '))
 
-quad_coords_list = []
+    for i in point:
+        if i[0] == 'map':
+            map_point.append(i)
+        elif i[0] == 'frame':
+            frame_point.append(i)
 
-for i in range(num):
-    quad_coords = {
-        "pixel": np.array([
-            [frame_point[i*4][1]  ,   frame_point[i*4][2]],  # Third lampost top right
-            [frame_point[i*4+1][1], frame_point[i*4+1][2]],  # Corner of white rumble strip top left
-            [frame_point[i*4+2][1], frame_point[i*4+2][2]],  # Corner of rectangular road marking bottom left
-            [frame_point[i*4+3][1], frame_point[i*4+3][2]]  # Corner of dashed line bottom right
-        ]),
-        "lonlat": np.array([
-            [map_point[i*4][1]  ,   map_point[i*4][2]],  # Third lampost top right
-            [map_point[i*4+1][1], map_point[i*4+1][2]],  # Corner of white rumble strip top left
-            [map_point[i*4+2][1], map_point[i*4+2][2]],  # Corner of rectangular road marking bottom left
-            [map_point[i*4+3][1], map_point[i*4+3][2]]  # Corner of dashed line bottom right
-        ])
-    }
-    quad_coords_list.append(quad_coords)
+    if not(len(map_point) % 4 == 0 and len(frame_point) % 4 == 0):
+        print('Point Error')
+        sys.exit()
 
-print(quad_coords_list)
-#PixelMapper로 값 전달
+    quad_coords_list = []
 
-for i in range(num):
-##############변경해야하는 부분#######################
-# 좌표값을 받아야함(하나씩)
-    file = open(os.path.join(temp_path,"result"+str(i)+".txt"), 'r')
-    globals()['frame{}'.format(i)], globals()['point{}'.format(i)] = save_dict(file)
-
-
-map = cv2.imread(map_path, -1)
-for i in range(num):
-    globals()['BEV_Point{}'.format(i)] = dict()
-
-for frames in range(1, int(globals()['frame{}'.format(0)])):
     for i in range(num):
-        pm = PixelMapper(quad_coords_list[i]["pixel"], quad_coords_list[i]["lonlat"])
-        if globals()['point{}'.format(i)].get(str(frames)) != None:
-            for label in globals()['point{}'.format(i)].get(str(frames)) :
+        quad_coords = {
+            "pixel": np.array([
+                [frame_point[i*4][1]  ,   frame_point[i*4][2]],  # Third lampost top right
+                [frame_point[i*4+1][1], frame_point[i*4+1][2]],  # Corner of white rumble strip top left
+                [frame_point[i*4+2][1], frame_point[i*4+2][2]],  # Corner of rectangular road marking bottom left
+                [frame_point[i*4+3][1], frame_point[i*4+3][2]]  # Corner of dashed line bottom right
+            ]),
+            "lonlat": np.array([
+                [map_point[i*4][1]  ,   map_point[i*4][2]],  # Third lampost top right
+                [map_point[i*4+1][1], map_point[i*4+1][2]],  # Corner of white rumble strip top left
+                [map_point[i*4+2][1], map_point[i*4+2][2]],  # Corner of rectangular road marking bottom left
+                [map_point[i*4+3][1], map_point[i*4+3][2]]  # Corner of dashed line bottom right
+            ])
+        }
+        quad_coords_list.append(quad_coords)
+
+    print(quad_coords_list)
+    #PixelMapper로 값 전달
+
+
+    fileidx = 0
+    for inputfile in os.listdir(input_path):
+        name, ext = os.path.splitext(inputfile)
+
+        # 확장자가 .mp4인 경우
+        if ext == ".mp4":
+            # Output Folder에 저장된 MOT Result Text 읽기
+            print('read', original_output_path / (name + '.txt'))
+
+            ##############변경해야하는 부분#######################
+            # 좌표값을 받아야함(하나씩)
+            file = open(original_output_path / (name + '.txt'), 'r')
+            globals()['frame{}'.format(fileidx)], globals()['point{}'.format(fileidx)] = save_dict(file)
+            fileidx += 1
+
+    map = cv2.imread(str(map_path), -1)
+
+    for i in range(num):
+        globals()['BEV_Point{}'.format(i)] = dict()
+
+    for frames in range(1, int(globals()['frame{}'.format(0)])):
+        for i in range(num):
+            pm = PixelMapper(quad_coords_list[i]["pixel"], quad_coords_list[i]["lonlat"])
+            if globals()['point{}'.format(i)].get(str(frames)) != None:
+                for label in globals()['point{}'.format(i)].get(str(frames)) :
+                    uv = (label[1], label[2])
+                    lonlat = list(pm.pixel_to_lonlat(uv))
+                    li = [label[0], int(lonlat[0][0]), int(lonlat[0][1])]
+                    if frames in globals()['BEV_Point{}'.format(i)]:
+                        line = globals()['BEV_Point{}'.format(i)].get(frames)
+                        line.append(li)
+                    else:
+                        globals()['BEV_Point{}'.format(i)][frames] = [li]
+
+                    color = getcolor(abs(label[0]))
+                    cv2.circle(map, (int(lonlat[0][0]), int(lonlat[0][1])), 3, color, -1)
+
+            src = os.path.join(output_path, str(frames) + '.jpg')
+            cv2.imwrite(src, map)
+
+    ## HeatMap ##
+
+    # df = pd.DataFrame(index=range(0, 10), columns=range(0, 13))
+    df = [[0 for col in range(13)] for row in range(10)]
+
+    # df = df.fillna(0)
+
+    print(type(map.shape))
+    print(map.shape[1])
+
+    for frames in range(1, int(globals()['frame{}'.format(0)])):
+
+        for i in range(num):
+
+            if globals()['BEV_Point{}'.format(i)].get(frames) is not None:
+
+                for label in globals()['BEV_Point{}'.format(i)].get(frames):
+                    if label[2] < 0 or label[1] < 0 or label[1] > map.shape[1] or label[2] > map.shape[0] :
+
+                        continue
+
+                    x = round(int(label[2]) / map.shape[0] * 9)
+                    y = round(int(label[1]) / map.shape[1] * 12)
+                    df[x][y] += 1
+
+    print(df)
+
+    sns.heatmap(df, linewidths=0.1, linecolor="black")
+
+    plt.savefig(heatmap_path)
+
+'''
+id 라벨값에 맞춰 색깔을 지정하는 function
+'''
+def getcolor(idx):
+    idx = idx * 3
+    return (37 * idx) % 255, (17 * idx) % 255, (29 * idx) % 255
+
+'''
+실제공간과 도면을 mapping해주는 class
+'''
+class PixelMapper(object):
+    """
+    Create an object for converting pixels to geographic coordinates,
+    using four points with known locations which form a quadrilteral in both planes
+    Parameters
+    ----------
+    pixel_array : (4,2) shape numpy array
+        The (x,y) pixel coordinates corresponding to the top left, top right, bottom right, bottom left
+        pixels of the known region
+    lonlat_array : (4,2) shape numpy array
+        The (lon, lat) coordinates corresponding to the top left, top right, bottom right, bottom left
+        pixels of the known region
+    """
+
+    def __init__(self, pixel_array, lonlat_array):
+        assert pixel_array.shape == (4, 2), "Need (4,2) input array"
+        assert lonlat_array.shape == (4, 2), "Need (4,2) input array"
+        self.M = cv2.getPerspectiveTransform(np.float32(pixel_array), np.float32(lonlat_array))
+        self.invM = cv2.getPerspectiveTransform(np.float32(lonlat_array), np.float32(pixel_array))
+
+    #실제 공간을 도면으로 바꿈
+    def pixel_to_lonlat(self, pixel):
+        """
+        Convert a set of pixel coordinates to lon-lat coordinates
+        Parameters
+        ----------
+        pixel : (N,2) numpy array or (x,y) tuple
+            The (x,y) pixel coordinates to be converted
+        Returns
+        -------
+        (N,2) numpy array
+            The corresponding (lon, lat) coordinates
+        """
+        if type(pixel) != np.ndarray:
+            pixel = np.array(pixel).reshape(1, 2)
+        assert pixel.shape[1] == 2, "Need (N,2) input array"
+        pixel = np.concatenate([pixel, np.ones((pixel.shape[0], 1))], axis=1)
+        lonlat = np.dot(self.M, pixel.T)
+
+        return (lonlat[:2, :] / lonlat[2, :]).T
+    #도면 공간을 실제 공간으로 바꿈
+    def lonlat_to_pixel(self, lonlat):
+        """
+        Convert a set of lon-lat coordinates to pixel coordinates
+        Parameters
+        ----------
+        lonlat : (N,2) numpy array or (x,y) tuple
+            The (lon,lat) coordinates to be converted
+        Returns
+        -------
+        (N,2) numpy array
+            The corresponding (x, y) pixel coordinates
+        """
+        if type(lonlat) != np.ndarray:
+            lonlat = np.array(lonlat).reshape(1, 2)
+        assert lonlat.shape[1] == 2, "Need (N,2) input array"
+        lonlat = np.concatenate([lonlat, np.ones((lonlat.shape[0], 1))], axis=1)
+        pixel = np.dot(self.invM, lonlat.T)
+
+        return (pixel[:2, :] / pixel[2, :]).T
+
+"""
+lonlat에 frame을 한번에 저장하는 function
+"""
+def save_lonlat_frame(point, pm,frame_num ,input_dir, output_dir):
+    map = cv2.imread(input_dir, -1)
+
+    #1541
+    for frames in range(1, frame_num): #object ID마다 색깔바꿔서 점찍기
+        if point.get(str(frames)) != None:
+            for label in point.get(str(frames)) :
                 uv = (label[1], label[2])
                 lonlat = list(pm.pixel_to_lonlat(uv))
-                li = [label[0], int(lonlat[0][0]), int(lonlat[0][1])]
-                if frames in globals()['BEV_Point{}'.format(i)]:
-                    line = globals()['BEV_Point{}'.format(i)].get(frames)
-                    line.append(li)
-                else:
-                    globals()['BEV_Point{}'.format(i)][frames] = [li]
-
                 color = getcolor(abs(label[0]))
                 cv2.circle(map, (int(lonlat[0][0]), int(lonlat[0][1])), 3, color, -1)
 
-        src = os.path.join(output_path, str(frames) + '.jpg')
+        src = os.path.join(output_dir, str(frames)+'.jpg')
         cv2.imwrite(src, map)
 
-# heatmap
+def save_dict(file):
+    ##################################################
+    frame = 0
+    point = dict()
+    while True:
+        line = file.readline()
 
-import pandas as pd
+        if not line:
+            break
 
-import seaborn as sns
+        info = line[:-1].split(" ")
 
-import matplotlib.pyplot as plt
+        frame = info[0]
 
-import numpy as np
+        if info[0] in point:
+            line = point.get(info[0])
+            line.append(list(map(int, info[1:])))
+        else:
+            point[info[0]] = [list(map(int, info[1:]))]
 
+    file.close()
 
-# df = pd.DataFrame(index=range(0, 10), columns=range(0, 13))
-df = [[0 for col in range(13)] for row in range(10)]
-
-# df = df.fillna(0)
-
-print(type(map.shape))
-print(map.shape[1])
-
-for frames in range(1, int(globals()['frame{}'.format(0)])):
-
-    for i in range(num):
-
-        if globals()['BEV_Point{}'.format(i)].get(frames) is not None:
-
-            for label in globals()['BEV_Point{}'.format(i)].get(frames):
-                if label[2] < 0 or label[1] < 0 or label[1] > map.shape[1] or label[2] > map.shape[0] :
-
-                    continue
-
-                x = round(int(label[2]) / map.shape[0] * 9)
-                y = round(int(label[1]) / map.shape[1] * 12)
-                df[x][y] += 1
-
-print(df)
-
-sns.heatmap(df, linewidths=0.1, linecolor="black")
-
-plt.savefig(heatmap_path)
+    return frame, point
+    ###########################################################################
--- startall.py
+++ startall.py
@@ -1,15 +1,30 @@
-# FastMOT 작업 후 바로 BEV 실행
-# Frame 저장 위치는 ./output/frame으로 설정해야 함
+# FastMOT + BEV All-in-One Script
+# Usage : python3 startall.py -i ./input -o ./output -m ./input/311_maps.png
+# 필수 옵션 : -i, -o, -m
+# -i (--input-uri) : Input Video 들어있는 경로
+# -o (--output-uri) : Output 폴더 경로
+# -m (--map-uri) : BEV에서 사용할 Map Image File 경로
+# -c (--config) : FastMOT Custom Config File 설정 (기본은 cfg/mot.json)
+# -s (--show) : FastMOT 동작 중 처리되는 영상 보기
+# -sm (--skip-mot) : FastMOT 생략 (이미 FastMOT Output Video가 있는 경우에 사용)
+# -sp (--skip-point) : Point 선택 생략 (이미 BEV Point 선택 텍스트 파일 (temp/points.txt) 있는 경우에 사용)
 
 import argparse
 import json
 import logging
 import os
+import traceback
+
 import fastmot
 import cv2
 from fastmot.utils import ConfigDecoder, Profiler
 from pathlib import Path
 from types import SimpleNamespace
+
+from BEV import mouse_point
+from BEV import BEV
+from BEV import output_video
+
 
 def start():
     #### Arguments Setting ####
@@ -18,19 +33,26 @@ def start():
     required = parser.add_argument_group('required arguments')
     group = parser.add_mutually_exclusive_group()
     required.add_argument('-i', '--input-uri', metavar="URI", required=True,
-                          help='Input Video Folder')
+                          help='Input Video Folder',
+                          default=Path(__file__).parent / 'input')
     required.add_argument('-o', '--output-uri', metavar="URI",
-                          help='Output File (Video, Text, Frame) Folder')
+                          help='Output File (Video, Text, Frame) Folder',
+                          default=Path(__file__).parent / 'output')
     required.add_argument('-m', '--map-uri', metavar="URI",
                           help='Map Image File URI')
     optional.add_argument('-c', '--config', metavar="FILE",
                           default=Path(__file__).parent / 'cfg' / 'mot.json',
                           help='path to JSON configuration file')
     optional.add_argument('-s', '--show', action='store_true', help='show visualizations')
+    optional.add_argument('-sm', '--skip-mot', action='store_true', help='Skip FastMOT')
+    optional.add_argument('-sp', '--skip-point', action='store_true', help='Skip Select Point')
     group.add_argument('-q', '--quiet', action='store_true', help='reduce output verbosity')
     group.add_argument('-v', '--verbose', action='store_true', help='increase output verbosity')
     parser._action_groups.append(optional)
     args = parser.parse_args()
+
+    if args.skip_mot and args.show:
+        raise parser.error('argument -s/--show: not allowed with argument -sm/--skip-mot')
 
     # set up logging
     logging.basicConfig(format='%(asctime)s [%(levelname)8s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
@@ -42,15 +64,15 @@ def start():
     else:
         logger.setLevel(logging.INFO)
 
-
     # load config file
     with open(args.config) as cfg_file:
         config = json.load(cfg_file, cls=ConfigDecoder, object_hook=lambda d: SimpleNamespace(**d))
 
-    #### Run FastMOT ####
-
     # Input 경로에 있는 모든 파일 읽기
     videolist = os.listdir(args.input_uri)
+
+    # Temp 폴더 생성
+    Path(Path(__file__).parent / 'temp').mkdir(parents=True, exist_ok=True)
 
     # 모든 File 읽기 위해 Loop
     for videofile in videolist:
@@ -59,61 +81,88 @@ def start():
 
         # 확장자가 .mp4인 경우
         if ext == ".mp4":
-            # FastMOT 실행
+            # MOT 작업을 Skip하지 않은 경우
+            if not args.skip_mot:
+                # FastMOT 실행
+                stream = fastmot.VideoIO(config.resize_to, args.input_uri + '/' + videofile, args.output_uri, **vars(config.stream_cfg))
+                mot = fastmot.MOT(config.resize_to, **vars(config.mot_cfg), draw=True)
+                mot.reset(stream.cap_dt)
 
-            stream = fastmot.VideoIO(config.resize_to, args.input_uri + '/' + videofile, args.output_uri, **vars(config.stream_cfg))
-            mot = fastmot.MOT(config.resize_to, **vars(config.mot_cfg), draw=True)
-            mot.reset(stream.cap_dt)
+                Path(args.output_uri).parent.mkdir(parents=True, exist_ok=True)
+                txt = open(args.output_uri + '/' + name + '.txt', 'w')
 
-            Path(args.output_uri).parent.mkdir(parents=True, exist_ok=True)
-            txt = open(args.output_uri + '/' + name + '.txt', 'w')
+                Path(args.output_uri + '/frame/' + name + '/').mkdir(parents=True, exist_ok=True)
+                framecount = 0
 
-            Path(args.output_uri + '/frame/' + name + '/').mkdir(parents=True, exist_ok=True)
-            framecount = 0
+                if args.show:
+                    cv2.namedWindow('Video', cv2.WINDOW_AUTOSIZE)
 
-            if args.show:
-                cv2.namedWindow('Video', cv2.WINDOW_AUTOSIZE)
-
-            logger.info('Starting video capture... ({})'.format(videofile))
-            stream.start_capture()
-            try:
-                with Profiler('app') as prof:
-                    while not args.show or cv2.getWindowProperty('Video', 0) >= 0:
-                        frame = stream.read()
-                        if frame is None:
-                            break
-
-                        mot.step(frame)
-
-                        for track in mot.visible_tracks():
-                            tl = track.tlbr[:2] / config.resize_to * stream.resolution
-                            br = track.tlbr[2:] / config.resize_to * stream.resolution
-                            w, h = br - tl + 1
-
-                            # New Text Format
-                            txt.write(
-                                f'{mot.frame_count} {track.trk_id} {int(tl[0] + w / 2)} {int((tl[1] + h) - 10)}\n')
-
-                        if args.show:
-                            cv2.imshow('Video', frame)
-                            if cv2.waitKey(1) & 0xFF == 27:
+                logger.info('Starting video capture... ({})'.format(videofile))
+                stream.start_capture()
+                try:
+                    with Profiler('app') as prof:
+                        while not args.show or cv2.getWindowProperty('Video', 0) >= 0:
+                            frame = stream.read()
+                            if frame is None:
                                 break
 
-                        cv2.imwrite("{}/{}.jpg".format(Path(args.output_uri + '/frame/' + name + '/'), framecount), frame)
-                        framecount += 1
-                        stream.write(frame)
-            finally:
-                # clean up resources
-                if txt is not None:
-                    txt.close()
-                stream.release()
-                cv2.destroyAllWindows()
+                            mot.step(frame)
 
-                avg_fps = round(mot.frame_count / prof.duration)
-                logger.info('Average FPS: %d', avg_fps)
-                mot.print_timing_info()
+                            for track in mot.visible_tracks():
+                                tl = track.tlbr[:2] / config.resize_to * stream.resolution
+                                br = track.tlbr[2:] / config.resize_to * stream.resolution
+                                w, h = br - tl + 1
 
-        #### Run BEV ####
+                                # New Text Format
+                                txt.write(
+                                    f'{mot.frame_count} {track.trk_id} {int(tl[0] + w / 2)} {int((tl[1] + h) - 10)}\n')
+
+                            if args.show:
+                                cv2.imshow('Video', frame)
+                                if cv2.waitKey(1) & 0xFF == 27:
+                                    break
+
+                            cv2.imwrite("{}/{}.jpg".format(Path(args.output_uri + '/frame/' + name + '/'), framecount), frame)
+                            framecount += 1
+                            stream.write(frame)
+                finally:
+                    # clean up resources
+                    if txt is not None:
+                        txt.close()
+                    stream.release()
+                    cv2.destroyAllWindows()
+
+                    avg_fps = round(mot.frame_count / prof.duration)
+                    logger.info('Average FPS: %d', avg_fps)
+                    mot.print_timing_info()
+            else:
+                logger.info('Skip MOT...')
+
+    try:
+        # Point 지정 시작
+        '''
+        pixel : 실제 공간
+        lonloat : 도면 공간
+        실제 mapping 되는 곳에 좌표를 입력 @@@.py 사용
+        오른쪽 위, 왼쪽 위, 왼쪽 아래, 오른쪽 아래 순서
+        '''
+
+        if not args.skip_point:
+            logger.info('Waiting Select Points...')
+            mouse_point.start(Path(__file__).parent / 'temp/points.txt', Path(args.output_uri + '/frame/'), args.map_uri)
+
+        # BEV Start
+        logger.info('Start BEV...')
+        BEV.start(Path(args.input_uri), Path(args.output_uri), Path(args.map_uri).absolute())
+
+        # Output Video Start
+        logger.info('Start Output Video...')
+        output_video.start(Path(args.output_uri))
+
+        logger.info('Finished!')
+    except:
+        logger.error(traceback.format_exc())
+
 
 if __name__ == '__main__':
     start()
