24066b9f56030478d687a8a27d11c6ceaebd6335
Aftermoon-dev <ddol0225@gmail.com>
2021-11-24 22:30:50 +0900

    Updated DDL

--- DB/newmotdb.sql
+++ DB/newmotdb.sql
@@ -1,20 +1,26 @@
 CREATE DATABASE IF NOT EXISTS `inc_mot` default character set utf8mb4;
 use `inc_mot`;
 
-CREATE TABLE IF NOT EXISTS `video` (
-	id INT PRIMARY KEY auto_increment,
-    videoFileName varchar(512) not null unique
-);
-
 CREATE TABLE IF NOT EXISTS `videoGroup` (
 	id INT PRIMARY KEY auto_increment,
-    video_id INT NOT NULL,
-    FOREIGN KEY (video_id) REFERENCES video(id)
+    createAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updateAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS `video` (
+	id INT PRIMARY KEY auto_increment,
+    videoFileName varchar(512) not null unique,
+    videoGroup_id INT NOT NULL,
+    createAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updateAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (videoGroup_id) REFERENCES videoGroup(id)
 );
 
 CREATE TABLE IF NOT EXISTS `frameinfo` (
 	frame_id INT PRIMARY KEY,
     video_id INT NOT NULL,
+    createAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updateAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (video_id) REFERENCES video(id)
 );
 
@@ -24,6 +30,8 @@ CREATE TABLE IF NOT EXISTS `trackinginfo` (
     frameinfo_video_id INT NOT NULL,
     x DOUBLE NOT NULL,
     y DOUBLE NOT NULL,
+    createAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updateAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY(identifyID, frameinfo_frame_id),
     FOREIGN KEY(frameinfo_frame_id) REFERENCES frameinfo(frame_id),
     FOREIGN KEY(frameinfo_video_id) REFERENCES frameinfo(video_id)
@@ -35,6 +43,8 @@ CREATE TABLE IF NOT EXISTS `globalmapping` (
     y DOUBLE NOT NULL,
     frameinfo_frame_id INT NOT NULL,
     videoGroup_id INT NOT NULL,
+    createAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updateAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (frameinfo_frame_id) REFERENCES frameinfo(frame_id),
 	FOREIGN KEY (videoGroup_id) REFERENCES videoGroup(id)
 );
@@ -43,6 +53,8 @@ CREATE TABLE IF NOT EXISTS `globalmapping_has_trackinginfo` (
 	globalmapping_globalID INT NOT NULL,
     trackinginfo_identifyID INT NOT NULL,
     trackinginfo_frameinfo_frame_id INT NOT NULL,
+    createAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updateAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (globalmapping_globalID, trackinginfo_identifyID, trackinginfo_frameinfo_frame_id),
     FOREIGN KEY (globalmapping_globalID) REFERENCES globalmapping(globalID),
     FOREIGN KEY (trackinginfo_identifyID) REFERENCES trackinginfo(identifyID),
