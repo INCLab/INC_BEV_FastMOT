499df70c605672ec215a6268bb2f060ffb50b6c2
Jungik Jang <51084152+cheeseBG@users.noreply.github.com>
2022-04-12 18:22:59 +0900

    add 5person data

--- DTW/read_data.py
+++ DTW/read_data.py
@@ -6,7 +6,8 @@ import json
 
 test_person = 5
 data_path = "data/5person"
-df = pd.read_excel(os.path.join(data_path, 'no_skip.xlsx'), usecols=[2,3], header=None)
+skip = 'skip10'
+df = pd.read_excel(os.path.join(data_path, skip + '.xlsx'), usecols=[2, 3], header=None)
 
 # 빈 셀에는 -1로 채우기 -> 후처리 편하게 하기위해
 df.fillna(-1, inplace=True)
@@ -63,7 +64,7 @@ for data in df[3].to_list():
     else:
         tar += 1
 
-file_path = os.path.join(data_path, 'no_skip.json')
+file_path = os.path.join(data_path, skip + '.json')
 
 with open(file_path, 'w') as outfile:
     json.dump(data_dict, outfile, indent=4)
@@ -72,47 +73,3 @@ with open(file_path) as f:
     json_data = json.load(f)
 
 print(json_data['1'])
-
-
-
-# json_name = 'no_skip.json'
-# data_path = 'data/' + json_name
-#
-# # Read json data
-# with open(data_path) as f:
-#     json_data = json.load(f)
-#
-# result_list = []
-#
-# for data_num in range(1,51):
-#     test_case_dict = json_data[str(data_num)]
-#
-#     cam_name = ['ch01', 'ch02', 'ch03', 'ch04']
-#     tar_name = ['tar1', 'tar2', 'tar3', 'delete']
-#
-#     # Create local mapping list with json data
-#     tar_list = [[], [], []]
-#
-#     for cam in cam_name:
-#         if cam == 'ch01':
-#             start_id = 10000
-#         elif cam == 'ch02':
-#             start_id = 20000
-#         elif cam == 'ch03':
-#             start_id = 30000
-#         elif cam == 'ch04':
-#             start_id = 40000
-#
-#         for tar in tar_name:
-#             if tar == 'delete':
-#                 break
-#             elif tar == 'tar1' and test_case_dict[cam][tar]:
-#                 tar_list[0].append(start_id + test_case_dict[cam][tar][0])
-#             elif tar == 'tar2' and test_case_dict[cam][tar]:
-#                 tar_list[1].append(start_id + test_case_dict[cam][tar][0])
-#             elif tar == 'tar3' and test_case_dict[cam][tar]:
-#                 tar_list[2].append(start_id + test_case_dict[cam][tar][0])
-#     result_list.append(tar_list)
-#
-# result_df = pd.DataFrame(result_list)
-# result_df.to_excel('gt.xlsx')
